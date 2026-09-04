b415722fd48b989e657f74b2415387a8bac98ded
lijun <346181277@QQ.COM>
2026-09-04 22:32:37 +0800

    更新 app.js

--- app.js
+++ app.js
@@ -1634,8 +1634,8 @@ notes = uniqueNotes;
 
                     ${
                         note.favorite
-                        ? "⭐"
-                        : "⭐"
+                        ? "⭐ 收藏"
+                        : "⭐ 取消收藏"
                     }
 
                 </button>
